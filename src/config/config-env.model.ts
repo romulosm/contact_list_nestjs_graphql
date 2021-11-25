d273b4e2d6bafde32f55a1aef86fcb6ae8ce28af
Rômulo Machado <romulo.machado@meta.com.br>
2021-11-25 19:46:53 -0300

    init project

---
+++ src/config/config-env.model.ts
@@ -0,0 +1,21 @@
+import { IsString, IsNumber, IsPositive, IsEnum } from 'class-validator';
+
+export enum NodeEnv {
+  Development = 'development',
+  Production = 'production',
+  Test = 'test',
+}
+
+export class ConfigEnv {
+  @IsNumber()
+  @IsPositive()
+  httpPort: number;
+
+  @IsString()
+  @IsEnum(NodeEnv)
+  nodeEnv: string;
+
+  get isProduction(): boolean {
+    return this.nodeEnv === NodeEnv.Production;
+  }
+}
